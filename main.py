45cee0bc89ced0c8a456598cb386e392ceba7fa2
kratuspython <117849549+kratuspython@users.noreply.github.com>
2023-01-24 13:23:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
 
 #----------------------- APP --------------------------------#
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 #------------------------------------------------------------#
@@ -44,7 +45,7 @@ def load_user(user_id):
 #--------------------------------------------------------------#
 
 #----------------------- CONNECT TO DB --------------------------------#
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 #----------------------------------------------------------------------#
